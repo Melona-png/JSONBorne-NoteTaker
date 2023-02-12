2b511e3ffe87ba76b61f8b30841c3d72c1fa3efc
Melona-Png <madisont.woodward@gmail.com>
2023-02-11 23:03:55 -0500

    functional. working as intended pushing to main

--- server.js
+++ server.js
@@ -15,21 +15,24 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.get('/api/notes', (req, res) => res.json(notes));
+app.get('/api/notes', (req, res) => {fs.readFile('./db/db.json' , (err,data) => {
+  res.json(JSON.parse(data))
+})
+});
 
 app.post("/api/notes", (req, res) => {
     const newNote = req.body;
     newNote.id = uuid.v4();
     notes.push(newNote);
   
-    fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes), (err) => {
+    fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes), (err) => {
       if (err) throw err;
       res.send(newNote);
     });
