79b1213a6ba791e90e625b6cb00b7dc95dfbee9b
Melona-Png <madisont.woodward@gmail.com>
2023-02-11 22:25:19 -0500

    trying to fix noteshtml connection

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
-//vars
 const express = require('express');
-//require json file
-const db = require('./db/db.json')
+const path = require('path');
+const dbData = require('./db/db.json');
+const uuid = require("uuid");
+const fs = require("fs");
+
+let notes = dbData;
+
 const PORT = 3001;
 
-// initialize
 const app = express();
-app.get('/', (req, res) => res.send('Visit http://localhost:3001/api'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/api/notes', (req, res) => res.json(notes));
+
+app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuid.v4();
+    notes.push(newNote);
+  
+    fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      res.send(newNote);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});
